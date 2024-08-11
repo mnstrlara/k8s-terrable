02d0a039d5e52fef34c9144b96d80b952147345a
Lara Manestar <lm@appon.net>
2024-08-11 18:31:24 +0200

    fix: network adapter for VMs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,21 @@ variable "vm_image" {
   type        = string
   sensitive   = true
 }
+
 variable "ssh_key_source" {
   description = "The source path of the SSH public key"
   type        = string
   sensitive   = true
 }
+
 variable "ssh_private_key_path" {
   description = "The path to the SSH private key file"
+  type        = string
+  sensitive   = true
+}
+
+variable "network_adapter" {
+  description = "The network adapter to use for the virtual machine"
   type        = string
   sensitive   = true
 }
